35ce065659934ed7fa87b117969b92b8ead06bc7
youtr-s <suzuki_yutaro@comture.com>
2019-03-08 10:06:13 +0900

    mod alert component

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,22 +1,39 @@
 <template>
-    <div class="v-kuc-alert">
-        <slot></slot>
+    <div class="v-kuc-alert"
+    　:class ="{ 'bg-danger': type === 'error', 'bg-success': type === 'success'}"
+    >
+        {{ text }}
     </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 export default {
+    props: {
+        text: {
+            type: String, required: true
+        },
+        type: {
+            type: String, default: 'error'
+        }  
+    }
+
 }
 </script>
 
 <style scoped>
 .v-kuc-alert {
     position: relative;
-  display: block;
-  margin: 8px 0;
-  padding: 4px 18px;
-  background: #e74c3c;
-  color: #f6f6f6;
+    display: block;
+    margin: 8px 0;
+    padding: 4px 18px;
+    background: #e74c3c;
+    color: #f6f6f6;
+}
+.v-kuc-alert.bg-danger {
+    background: #e74c3c;
+}
+.v-kuc-alert.bg-success {
+    background: #91C36C;
 }
 </style>
